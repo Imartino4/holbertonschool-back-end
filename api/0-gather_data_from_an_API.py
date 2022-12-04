2b1af63fc517de269a1401e02bc128c35682b2eb
Imartino4 <ignacio.martacos@gmail.com>
2022-12-04 19:14:58 +0000

    Task 0 - trying to fix "module not documented"

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -4,29 +4,31 @@ import requests
 from sys import argv
 import json
 
-u_id = int(argv[1])
-# APIS url
-todo_url = "https://jsonplaceholder.typicode.com/todos"
-user_url = "https://jsonplaceholder.typicode.com/users"
-user = requests.get(user_url)
-todo = requests.get(todo_url)
 
-for u in user.json():
-    if u["id"] == u_id:
-        u_name = u["name"]
-        break
+if __name__ == '__main__':
+    u_id = int(argv[1])
+    # APIS url
+    todo_url = "https://jsonplaceholder.typicode.com/todos"
+    user_url = "https://jsonplaceholder.typicode.com/users"
+    user = requests.get(user_url)
+    todo = requests.get(todo_url)
 
-task_done = 0
-total_task = 0
-task_title = []
-for td in todo.json():
-    if td["userId"] == u_id:
-        total_task += 1
-        if td["completed"]:
-            task_done += 1
-            task_title.append(td["title"])
+    for u in user.json():
+        if u["id"] == u_id:
+            u_name = u["name"]
+            break
 
-print("Employee {} is donde with tasks({}/{})".format(
-            u_name, task_done, total_task))
-for title in task_title:
-    print("\t {}".format(title))
+    task_done = 0
+    total_task = 0
+    task_title = []
+    for td in todo.json():
+        if td["userId"] == u_id:
+            total_task += 1
+            if td["completed"]:
+                task_done += 1
+                task_title.append(td["title"])
+
+    print("Employee {} is donde with tasks({}/{})".format(
+                u_name, task_done, total_task))
+    for title in task_title:
+        print("\t {}".format(title))
