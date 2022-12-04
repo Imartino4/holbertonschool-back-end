fe4b07cd658e18dc6c222a71473c5a67f3bd94f8
Imartino4 <ignacio.martacos@gmail.com>
2022-12-04 22:19:36 +0000

    Task 1 - correct outputs

--- api/1-export_to_CSV.py
+++ api/1-export_to_CSV.py
@@ -25,27 +25,12 @@ if __name__ == '__main__':
             u_username = u["username"]
             break
 
-    task_done = 0
-    total_task = 0
-    task_title = []
-    for td in todo.json():
-        if td["userId"] == u_id:
-            total_task += 1
-            if td["completed"]:
-                task_done += 1
-                task_title.append(td["title"])
-
-    print("Employee {} is donde with tasks({}/{})".format(
-                u_name, task_done, total_task))
-    for title in task_title:
-        print("\t {}".format(title))
-
-file_name = f"{u_id}.csv"
-with open(file_name, 'w') as f:
-    writer = csv.writer(f, delimiter=',', quotechar='"', quoting=csv.QUOTE_ALL)
-    for td in todo.json():
-        if td["userId"] == u_id:
-            task_status = td["completed"]
-            task_title = td["title"]
-            writer.writerow([u_id, u_username, task_status, task_title])
+    file_name = "{}.csv".format(u_id)
+    with open(file_name, 'w') as f:
+        writer = csv.writer(f, delimiter=',', quotechar='"', quoting=csv.QUOTE_ALL)
+        for td in todo.json():
+            if td["userId"] == u_id:
+                task_status = td["completed"]
+                task_title = td["title"]
+                writer.writerow([u_id, u_username, task_status, task_title])
 
